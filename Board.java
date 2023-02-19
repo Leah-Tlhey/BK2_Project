ccb5ebc5a2fb6ce9c582f77580cf48d47fbaeecd
holly683 <74032781+holly683@users.noreply.github.com>
2023-02-19 19:14:29 +0800

    Update Board.java

--- Board.java
+++ Board.java
@@ -6,7 +6,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
